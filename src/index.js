becc31ebef7b6e516f736a140199541e21e6f9d1
Samuel priyam Mulpuri <87686877+Samuel-priyam@users.noreply.github.com>
2023-02-03 17:39:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
+    const {url} = req;
+    if(url === '/welcome'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Demo</title></head>')
+        res.write('<body> <p> Welcome to Dominos! </p></body>')
+        res.write('</html>')
+    }
+    else if(url === '/contact'){
+        res.setHeader('content-Type', 'application/json')
+        res.write('{')
+        res.write('phone:18602100000')
+        res.write('email: guestcaredominos@jublfood.com')
+        res.write('}')
+    }
+    // else{
+
+    // }
   
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
